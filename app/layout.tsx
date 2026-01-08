0941f9cd2f3a037e29d76e3dd1d73f4bcbaf49ab
oktochoi <okto0914@gmail.com>
2026-01-08 19:03:34 +0900

    seo challeng

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "바다거북스프게임",
   description: "추리와 질문으로 진실을 밝혀내는 바다거북스프 게임",
+  verification: {
+    google: "4j3cTkVACL2lF9s0CFfg6x9kHsVdndQdbKI5atxdBGQ",
+  },
 };
 
 export default function RootLayout({
